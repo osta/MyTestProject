6b129eb3f276f2f2b29aa6f607ea64b3e835d19d
osa2 <osa.tate@gmail.com>
2012-10-31 19:46:10 +0900

    The first commit

---
+++ src/test001/myhello.groovy
@@ -0,0 +1,11 @@
+package test001
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: osa2
+ * Date: 12/10/31
+ * Time: 19:44
+ * To change this template use File | Settings | File Templates.
+ */
+
+println "Hello GroovyTest"
